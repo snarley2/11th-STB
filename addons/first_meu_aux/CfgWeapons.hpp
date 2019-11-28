ec0b3c5e08ad4ee46e77c186409302324d100736
R. Chatter <octavewolfz@gmail.com>
2019-11-28 01:42:56 -0800

    All the Shotty's now!

--- addons/first_meu_aux/CfgWeapons.hpp
+++ addons/first_meu_aux/CfgWeapons.hpp
@@ -2,7 +2,6 @@ class CfgWeapons
 {
 
     class Rifle_Base_F;
-    class OPTRE_M45E;
     
     class OPTRE_M45: Rifle_Base_F
     {
@@ -12,14 +11,15 @@ class CfgWeapons
             "OPTRE_6rnd_8gauge_HumblerSlugs",
             "OPTRE_12Rnd_8Gauge_Flechettes",
             "OPTRE_6Rnd_8Gauge_Flechettes",
-            "OPTRE_12Rnd_8Gauge_Pellets" ,
-            "OPTRE_6Rnd_8Gauge_Pellets" ,
-            "OPTRE_12Rnd_8Gauge_Slugs" ,
-            "OPTRE_6Rnd_8Gauge_Slugs"
+             "OPTRE_12Rnd_8Gauge_Pellets",
+             "OPTRE_6Rnd_8Gauge_Pellets",
+             "OPTRE_12Rnd_8Gauge_Slugs",
+             "OPTRE_6Rnd_8Gauge_Slugs"
+            
         };
     };
-
-    class OPTRE_M45X: OPTRE_M45E
+    
+    class OPTRE_M45E: OPTRE_M45
     {
         magazines[] =
         {
@@ -31,6 +31,23 @@ class CfgWeapons
             "OPTRE_6Rnd_8Gauge_Pellets" ,
             "OPTRE_12Rnd_8Gauge_Slugs" ,
             "OPTRE_6Rnd_8Gauge_Slugs"
+            
+        };
+    };
+
+    class OPTRE_M45X: OPTRE_M45E 
+    {
+        magazines[] =
+        {
+            "OPTRE_12rnd_8gauge_humblerslugs",
+            "OPTRE_6rnd_8gauge_HumblerSlugs",
+            "OPTRE_12Rnd_8Gauge_Flechettes",
+            "OPTRE_6Rnd_8Gauge_Flechettes",
+            "OPTRE_12Rnd_8Gauge_Pellets" ,
+            "OPTRE_6Rnd_8Gauge_Pellets" ,
+            "OPTRE_12Rnd_8Gauge_Slugs" ,
+            "OPTRE_6Rnd_8Gauge_Slugs"
+            
         };
     };
 };
