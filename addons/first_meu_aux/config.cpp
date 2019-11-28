0a1c14d892245ed58473917c4a86545314554ba8
R. Chatter <octavewolfz@gmail.com>
2019-11-28 00:06:42 -0800

    Added Mortar Supply Drop

--- addons/first_meu_aux/config.cpp
+++ addons/first_meu_aux/config.cpp
@@ -1,40 +1,39 @@
 #define item_xx(a,b) class _xx_##a {name = a; count = b;}
 #define mag_xx(a,b) class _xx_##a {magazine = a; count = b;}
 #define weap_xx(a,b) class _xx_##a {weapon = a; count = b;}
+
 class cfgPatches
 {
     class first_meu_aux
     {
-		units[] = {"OPTRE_Ammo_SupplyPod_Launcher","OPTRE_Ammo_SupplyPod_Medical","OPTRE_Ammo_SupplyPod_LMG"};
+		units[] = {
+			"OPTRE_Ammo_SupplyPod_Launcher",
+			"OPTRE_Ammo_SupplyPod_Medical",
+			"OPTRE_Ammo_SupplyPod_LMG",
+			"OPTRE_Ammo_SupplyPod_Mortars"
+			};
         requiredVersion = 0.1;
-		requiredAddons[] = {"OPTRE_Core","OPTRE_Weapons","OPTRE_UNSC_Units","OPTRE_Ins_Units","A3_Data_F","OPTRE_Misc", "OPTRE_Misc_Crates"};
+		requiredAddons[] = {
+			"OPTRE_Core",
+			"OPTRE_Weapons",
+			"OPTRE_UNSC_Units",
+			"OPTRE_Ins_Units",
+			"A3_Data_F",
+			"OPTRE_Misc",
+			"OPTRE_Misc_Crates"
+			};
     };
 };
-class CfgWeapons
-{
-    class Rifle_Base_F;
-    class OPTRE_M45 : Rifle_Base_F
-	{
-		dlc = "OPTRE";
-		scope 										= 2;
-		handAnim[] 									= {"OFP2_ManSkeleton", "\OPTRE_Weapons\Shotgun\data\anim\benelli.rtm"};
-		model										= "\OPTRE_Weapons\Shotgun\shotgun.p3d";
-		displayName 									= "[UNSC] M45 Tactical Shotgun";
-		descriptionShort 								= "8 Gauge Shotgun";
-		picture = "\OPTRE_weapons\shotgun\icons\shotgun_black.paa";
-		pictureWire 								= "\OPTRE_Weapons\data\Pictures\WireWeaponIcons\Prime\Shotgun\Shotgun.paa";
-		magazines[] 								= {"OPTRE_6Rnd_8Gauge_Pellets","OPTRE_6Rnd_8Gauge_Slugs","OPTRE_6Rnd_8Gauge_Flechettes","OPTRE_6rnd_8gauge_humblerslugs","OPTRE_12Rnd_8Gauge_Flechettes","OPTRE_12Rnd_8Gauge_Pellets","OPTRE_12Rnd_8Gauge_Slugs","OPTRE_12rnd_8gauge_humblerslugs"};
-    }
-}
+
 class CfgVehicles
 {
 	class OPTRE_Ammo_SupplyPod_Empty;
     class OPTRE_Ammo_SupplyPod_Launcher: OPTRE_Ammo_SupplyPod_Empty
 	{
-		dlc = "OPTRE";
-		displayName = "[UNSC] Supply Pod (M41)";
-		model = "\OPTRE_misc\crates\Supply_pod.p3d";
-		author = "Article 2 Studios";
+		dlc 				= "OPTRE";
+		displayName			= "[UNSC] Supply Pod (M41)";
+		model 				= "\OPTRE_misc\crates\Supply_pod.p3d";
+		author 				= "Article 2 Studios";
 		class TransportMagazines
 		{
 			delete xx_OPTRE_M41_Twin_HEAT;
@@ -48,12 +47,13 @@ class CfgVehicles
 		};
 		class TransportItems{};
 	};
+
     class OPTRE_Ammo_SupplyPod_Medical: OPTRE_Ammo_SupplyPod_Empty
 	{
-		dlc = "OPTRE";
-		displayName = "[UNSC] Supply Pod (Medical)";
-		model = "\OPTRE_misc\crates\Supply_pod.p3d";
-		author = "Article 2 Studios";
+		dlc 				= "OPTRE";
+		displayName 		= "[UNSC] Supply Pod (Medical)";
+		model 				= "\OPTRE_misc\crates\Supply_pod.p3d";
+		author 				= "Article 2 Studios";
 		class TransportMagazines{};
 		class TransportWeapons{};
 		class TransportItems
@@ -75,6 +75,7 @@ class CfgVehicles
 			item_xx(OPTRE_MedKit, 10);
 		};
 	};
+
 	class OPTRE_Ammo_SupplyPod_LMG: OPTRE_Ammo_SupplyPod_Empty
 	{
 		class TransportMagazines
@@ -93,17 +94,24 @@ class CfgVehicles
 			item_xx(optic_arco_blk_f, 1)
 		};
 	};
-	class OPTRE_Ammo_SupplyPod_Medical: OPTRE_Ammo_SupplyPod_Empty
+
+	class OPTRE_Ammo_SupplyPod_ARC: OPTRE_Ammo_SupplyPod_Empty
 	{
+		dlc = "OPTRE";
+		displayName = "[UNSC] Supply Pod (Mortars)";
+		author = "Article 2 Studios";
+        
 		class TransportMagazines
 		{
-			delete xx_OPTRE_200Rnd_95x40_Box;
-			delete xx_OPTRE_200Rnd_95x40_Box_tracer;
-			mag_xx(OPTRE_400Rnd_762x51_Box_Tracer, 3);
-			mag_xx(OPTRE_100Rnd_762x51_Box_Tracer, 6);
+			delete xx_OPTRE_32Rnd_762x51_Mag_tracer;
+			delete xx_OPTRE_32Rnd_762x51_Mag;
+			mag_xx(ACE_1Rnd_82mm_Mo_HE,32);
+			mag_xx(ACE_1Rnd_82mm_Mo_Smoke,8);
+			mag_xx(ACE_1Rnd_82mm_Mo_Illum,8);
 		};
-		class TransportWeapons{};
+		class TransportWeapons{
+			delete xx_OPTRE_MA5C;
+		};
 		class TransportItems{};
-	};
-	
+	};	
 };
