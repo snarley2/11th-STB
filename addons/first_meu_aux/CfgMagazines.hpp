c81c7e5ad52ad7737d3b90feafe4fcbe213ac853
Mark Broome <dmarkbroome@gmail.com>
2019-11-29 06:29:43 +0000

    Patch 3

---
+++ addons/first_meu_aux/CfgMagazines.hpp
@@ -0,0 +1,46 @@
+class CfgMagazines
+{
+    class OPTRE_60Rnd_762x51_Mag;
+    class 150Rnd_762x51_Box;
+    
+    class OPTRE_100Rnd_762x51_Box: OPTRE_60Rnd_762x51_Mag
+	{
+		ace_isbelt = 1;
+	};
+	class OPTRE_100Rnd_762x51_Box_Tracer: OPTRE_100Rnd_762x51_Box
+	{
+		ace_isbelt = 1;
+	};
+	class OPTRE_100Rnd_762x51_Box_Tracer_Yellow: OPTRE_100Rnd_762x51_Box_Tracer
+	{
+		ace_isbelt = 1;
+	};
+	class OPTRE_400Rnd_762x51_Box_Tracer: OPTRE_100Rnd_762x51_Box_Tracer
+	{
+		ace_isbelt = 1;
+	};
+    class OPTRE_100Rnd_95x40_Box: 150Rnd_762x51_Box
+	{
+		ace_isbelt = 1;
+	};
+	class OPTRE_100Rnd_95x40_Box_Tracer: OPTRE_100Rnd_95x40_Box
+	{
+		ace_isbelt = 1;
+	};
+	class OPTRE_100Rnd_95x40_Box_Tracer_Yellow: OPTRE_100Rnd_95x40_Box_Tracer
+	{
+		ace_isbelt = 1;
+	};
+	class OPTRE_200Rnd_95x40_Box: OPTRE_100Rnd_95x40_Box
+	{
+		ace_isbelt = 1;
+	};
+	class OPTRE_200Rnd_95x40_Box_Tracer: OPTRE_200Rnd_95x40_Box
+	{
+		ace_isbelt = 1;
+	};
+	class OPTRE_200Rnd_95x40_Box_Tracer_Yellow: OPTRE_200Rnd_95x40_Box_Tracer
+	{
+		ace_isbelt = 1;
+	};
+};
