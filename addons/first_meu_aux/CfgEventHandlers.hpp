2ac595964e200aeed5790253fba753036a611121
J Mehew <jamesmehew@yahoo.ca>
2020-04-04 21:09:50 +0000

    Update CfgEventHandlers.hpp

--- addons/first_meu_aux/CfgEventHandlers.hpp
+++ addons/first_meu_aux/CfgEventHandlers.hpp
@@ -9,7 +9,7 @@ class Extended_InitPost_EventHandlers
 	};
 	class OPTRE_SupplyPod_TrackedVehicleRepair 
 	{
-		class OPTRE_SupplyPod_VehicleRepair_initpost_eh
+		class OPTRE_SupplyPod_TrackedVehicleRepair_initpost_eh
 		{
 			serverInit = "[_this # 0, 8, 'ACE_Track'] call ace_repair_fnc_addSpareParts";//"['ACE_Wheel', _this] call ace_cargo_fnc_loadItem";//"systemChat str _this"; //"[_this, 8, 'ACE_Wheel'] call ace_repair_fnc_addSpareParts";
 		};
