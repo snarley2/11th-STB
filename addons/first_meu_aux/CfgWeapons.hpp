f834012de43f0c49114e67836219255a202bf52f
Mark <dmarkbroome@gmail.com>
2020-04-05 00:05:38 -0500

    abandoned shotgun memes add jetpack LR

--- addons/first_meu_aux/CfgWeapons.hpp
+++ addons/first_meu_aux/CfgWeapons.hpp
@@ -5,11 +5,8 @@ class CfgWeapons
 
     class JetPack_LR: OPTRE_S12_SOLA_Jetpack
         {
-            displayname = "S12 SOLA LR"
+            displayname = "JetPack LR"
             tf_hasLRradio=1;
-            tf_encryptionCode="tf_west_radio_code";
-            tf_dialog="rt1523g_radio_dialog";
-            tf_subtype="digital_lr";
-            tf_range=25000;
+            tf_range=20000;
         };
 };
