c1a972756ec706a34ec145620fc4da6d7e000d3d
Mark <dmarkbroome@gmail.com>
2020-04-05 14:09:27 -0500

    SHE LIVES

--- addons/first_meu_aux/config.cpp
+++ addons/first_meu_aux/config.cpp
@@ -2,7 +2,6 @@
 #define mag_xx(a,b) class _xx_##a {magazine = a; count = b;}
 #define weap_xx(a,b) class _xx_##a {weapon = a; count = b;}
 #include "CfgVehicles.hpp"
-#include "CfgWeapons.hpp"
 #include "CfgEventHandlers.hpp"
 
 class cfgPatches
