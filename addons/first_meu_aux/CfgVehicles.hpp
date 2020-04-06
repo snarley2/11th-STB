b5554015801e8308221cd7a9d0b13a93b69bc28c
J Mehew <jamesmehew@yahoo.ca>
2020-04-06 03:48:40 +0000

    Replace CfgVehicles.hpp

--- addons/first_meu_aux/CfgVehicles.hpp
+++ addons/first_meu_aux/CfgVehicles.hpp
@@ -8,9 +8,6 @@ class CfgVehicles
     class OPTRE_UNSC_falcon_unarmed;
     class OPTRE_Pelican_armed;
     class OPTRE_Pelican_unarmed;
-    class B_APC_Tracked_01_CRV_F;
-    class B_T_APC_Tracked_01_CRV_F;
-    class OPTRE_S12_SOLA_Jetpack;
 
 	class OPTRE_UNSC_falcon: OPTRE_falcon_base
     {
@@ -99,18 +96,148 @@ class CfgVehicles
     {
         tf_range=30000;
     };
-        
-    class Vulkan_Repair_APC: B_APC_Tracked_01_CRV_F
+    class B_APC_Tracked_01_base_F;
+    class B_APC_Tracked_01_CRV_F: B_APC_Tracked_01_base_F
     {
-        displayname = "Vulkan Repair APC";
-        ace_repair_canRepair = 1; 
+	ace_cargo_space = 8;
+	ace_cargo_hasCargo = 1;		
     };
         
-    class Vulkan_Repair_APC_P: B_T_APC_Tracked_01_CRV_F
+    class B_T_APC_Tracked_01_CRV_F: B_APC_Tracked_01_CRV_F
     {
-        displayname = "Vulkan Repair APC (Pacific)";
-        ace_repair_canRepair = 1; 
+	ace_cargo_space = 8;
+	ace_cargo_hasCargo = 1;		
     };
-    
-    
+    class Pandur2_Medical_F;
+	class Pandur2_Medical_E: Pandur2_Medical_F
+	{
+		ace_medical_isMedicalVehicle = 1;
+		class TransportMagazines
+		{
+			delete _xx_30Rnd_65x39_caseless_mag;
+			delete _xx_100Rnd_65x39_caseless_mag;
+			delete _xx_HandGrenade;
+			delete _xx_MiniGrenade;
+			delete _xx_1Rnd_HE_Grenade_shell;
+			delete _xx_1Rnd_Smoke_Grenade_shell;
+			delete _xx_1Rnd_SmokeGreen_Grenade_shell;
+			delete _xx_1Rnd_SmokeOrange_Grenade_shell;
+			delete _xx_1Rnd_SmokeBlue_Grenade_shell;
+			delete _xx_SmokeShell;
+			delete _xx_SmokeShellGreen;
+			delete _xx_SmokeShellOrange;
+			delete _xx_SmokeShellBlue;
+			delete _xx_NLAW_F;
+			delete _xx_Titan_AT;
+			delete _xx_Titan_AP;
+			delete _xx_Titan_AA;
+		};
+		class TransportWeapons
+		{
+			delete _xx_arifle_MX_F;
+		};
+		class TransportItems
+		{
+			delete _xx_FirstAidKit;
+			item_xx(ACE_elasticBandage, 100);
+			item_xx(ACE_packingBandage, 80);
+			item_xx(ACE_plasmaIV_500, 40);
+			item_xx(ACE_epinephrine, 80);
+			item_xx(ACE_tourniquet, 80);
+			item_xx(ACE_quikclot, 80);
+			item_xx(ACE_morphine, 80);
+			item_xx(ACE_plasmaIV, 40);
+			item_xx(ACE_splint, 80);
+			item_xx(ACE_personalAidKit, 10);
+			item_xx(ACE_surgicalKit, 10);	
+		};
+	};
+
+	class Pandur2_Medical_D: Pandur2_Medical_F
+	{
+		ace_medical_isMedicalVehicle = 1;
+		class TransportMagazines
+		{
+			delete _xx_30Rnd_65x39_caseless_mag;
+			delete _xx_100Rnd_65x39_caseless_mag;
+			delete _xx_HandGrenade;
+			delete _xx_MiniGrenade;
+			delete _xx_1Rnd_HE_Grenade_shell;
+			delete _xx_1Rnd_Smoke_Grenade_shell;
+			delete _xx_1Rnd_SmokeGreen_Grenade_shell;
+			delete _xx_1Rnd_SmokeOrange_Grenade_shell;
+			delete _xx_1Rnd_SmokeBlue_Grenade_shell;
+			delete _xx_SmokeShell;
+			delete _xx_SmokeShellGreen;
+			delete _xx_SmokeShellOrange;
+			delete _xx_SmokeShellBlue;
+			delete _xx_NLAW_F;
+			delete _xx_Titan_AT;
+			delete _xx_Titan_AP;
+			delete _xx_Titan_AA;
+		};
+		class TransportWeapons
+		{
+			delete _xx_arifle_MX_F;
+		};
+		class TransportItems
+		{
+			delete _xx_FirstAidKit;
+			item_xx(ACE_elasticBandage, 100);
+			item_xx(ACE_packingBandage, 80);
+			item_xx(ACE_plasmaIV_500, 40);
+			item_xx(ACE_epinephrine, 80);
+			item_xx(ACE_tourniquet, 80);
+			item_xx(ACE_quikclot, 80);
+			item_xx(ACE_morphine, 80);
+			item_xx(ACE_plasmaIV, 40);
+			item_xx(ACE_splint, 80);
+			item_xx(ACE_personalAidKit, 10);
+			item_xx(ACE_surgicalKit, 10);	
+		};
+	};
+
+	class Pandur2_Medical_WD: Pandur2_Medical_F
+	{
+		ace_medical_isMedicalVehicle = 1;
+		class TransportMagazines
+		{
+			delete _xx_30Rnd_65x39_caseless_mag;
+			delete _xx_100Rnd_65x39_caseless_mag;
+			delete _xx_HandGrenade;
+			delete _xx_MiniGrenade;
+			delete _xx_1Rnd_HE_Grenade_shell;
+			delete _xx_1Rnd_Smoke_Grenade_shell;
+			delete _xx_1Rnd_SmokeGreen_Grenade_shell;
+			delete _xx_1Rnd_SmokeOrange_Grenade_shell;
+			delete _xx_1Rnd_SmokeBlue_Grenade_shell;
+			delete _xx_SmokeShell;
+			delete _xx_SmokeShellGreen;
+			delete _xx_SmokeShellOrange;
+			delete _xx_SmokeShellBlue;
+			delete _xx_NLAW_F;
+			delete _xx_Titan_AT;
+			delete _xx_Titan_AP;
+			delete _xx_Titan_AA;
+		};
+		class TransportWeapons
+		{
+			delete _xx_arifle_MX_F;
+		};
+		class TransportItems
+		{
+			delete _xx_FirstAidKit;
+			item_xx(ACE_elasticBandage, 100);
+			item_xx(ACE_packingBandage, 80);
+			item_xx(ACE_plasmaIV_500, 40);
+			item_xx(ACE_epinephrine, 80);
+			item_xx(ACE_tourniquet, 80);
+			item_xx(ACE_quikclot, 80);
+			item_xx(ACE_morphine, 80);
+			item_xx(ACE_plasmaIV, 40);
+			item_xx(ACE_splint, 80);
+			item_xx(ACE_personalAidKit, 10);
+			item_xx(ACE_surgicalKit, 10);	
+		};
+	};
 };
