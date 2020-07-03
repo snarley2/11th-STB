d1285dd1fab9a05b45d5fdf66c6961cef73fc83b
enragedpixel <dmarkbroome@gmail.com>
2020-07-03 17:57:04 -0500

    Update CfgVehicles.hpp
    adds LR bags

--- addons/first_meu_aux/CfgVehicles.hpp
+++ addons/first_meu_aux/CfgVehicles.hpp
@@ -383,4 +383,198 @@ class CfgVehicles
 	{
 		armor = 1000;
 	};
+};class CBA_Extended_EventHandlers;
+class CfgVehicles
+{
+	#include "CfgSupplyPods.hpp"
+
+    class Helicopter_Base_H;
+    class OPTRE_falcon_base;
+    class OPTRE_UNSC_falcon_unarmed;
+    class OPTRE_Pelican_armed;
+    class OPTRE_Pelican_unarmed;
+    class B_APC_Tracked_01_CRV_F;
+    class B_T_APC_Tracked_01_CRV_F;
+    class OPTRE_S12_SOLA_Jetpack;
+    class StaticMGWeapon;
+    class OPTRE_ANPRC_515;
+
+	class OPTRE_UNSC_falcon: OPTRE_falcon_base
+    {
+        tf_range=30000;
+         class pilotCamera
+        {
+            class OpticsIn
+            {
+                class Wide
+                {
+                    opticsDisplayName = "WFOV";
+                    initAngleX = 0;
+                    minAngleX = 0;
+                    maxAngleX = 0;
+                    initAngleY = 0;
+                    minAngleY = 0;
+                    maxAngleY = 0;
+                    initFov = "(75 / 120)";
+                    minFov = "(75 / 120)";
+                    maxFov = "(75 / 120)";
+                    directionStabilized = 1;
+                    visionMode[] = {"Normal","NVG","Ti"};
+                    thermalMode[] = {0,1};
+                    gunnerOpticsModel = "\A3\Drones_F\Weapons_F_Gamma\Reticle\UAV_Optics_Gunner_wide_F.p3d";
+                    opticsPPEffects[] = {"OpticsCHAbera2","OpticsBlur2"};
+                };
+                class Medium: Wide
+                {
+                    opticsDisplayName = "MFOV";
+                    initFov = "(14.4 / 120)";
+                    minFov = "(14.4 / 120)";
+                    maxFov = "(14.4 / 120)";
+                    gunnerOpticsModel = "\A3\Drones_F\Weapons_F_Gamma\Reticle\UAV_Optics_Gunner_medium_F.p3d";
+                };
+                class Narrow: Wide
+                {
+                    opticsDisplayName = "NFOV";
+                    initFov = "(4.8 / 120)";
+                    minFov = "0.01";
+                    maxFov = "(4.8 / 120)";
+                    gunnerOpticsModel = "\A3\Drones_F\Weapons_F_Gamma\Reticle\UAV_Optics_Gunner_narrow_F.p3d";
+                };
+            };
+            minTurn = -90;
+            maxTurn = 90;
+            initTurn = 0;
+            minElev = -10;
+            maxElev = 90;
+            initElev = 25;
+            maxXRotSpeed = 1;
+            maxYRotSpeed = 1;
+            maxMouseXRotSpeed = 0.5;
+            maxMouseYRotSpeed = 0.5;
+            pilotOpticsShowCursor = 1;
+            controllable = 1;
+        };
+        memoryPointDriverOptics = "gunnerview";
+    };
+    
+	class OPTRE_Pelican_F: Helicopter_Base_H
+    {
+        tf_range=30000;
+    };
+    
+    class OPTRE_Hornet_base: Helicopter_Base_H
+    {
+        tf_range=30000;
+    };
+    
+	class VES_UH144_A: OPTRE_UNSC_falcon
+    {
+        tf_range=30000;
+    };
+    
+	class VES_UH144: OPTRE_UNSC_falcon_unarmed
+    {
+        tf_range=30000;
+    };
+    
+	class VES_D77HTCI_A: OPTRE_Pelican_armed
+    {
+        tf_range=30000;
+    };
+    
+	class VES_D77HTCI: OPTRE_Pelican_unarmed
+    {
+        tf_range=30000;
+    };
+        
+    class Vulkan_Repair_APC: B_APC_Tracked_01_CRV_F
+    {
+        displayname = "Vulkan Repair APC";
+        ace_repair_canRepair = 1; 
+    };
+        
+    class Vulkan_Repair_APC_P: B_T_APC_Tracked_01_CRV_F
+    {
+        displayname = "Vulkan Repair APC (Pacific)";
+        ace_repair_canRepair = 1; 
+    };
+    
+    class OPTRE_HEV: StaticMGWeapon
+	{
+        class TransportBackpacks
+        {
+            class xx_OPTRE_OPTRE_ILCS_HEAVY
+			{
+				backpack = "OPTRE_ILCS_Rucksack_Heavy";
+				count = 1;
+			};
+        }
+		class TransportMagazines
+		{
+			mag_xx(OPTRE_M9_Frag, 3);
+            mag_xx(ACE_M84, 2);
+            mag_xx(OPTRE_M2_Smoke_Orange, 1);
+            mag_xx(SmokeShell, 5);
+            mag_xx(C7_Remote_Mag, 1);
+            mag_xx(OPTRE_200Rnd_95x40_Box_HPSAPT, 1);
+            mag_xx(OPTRE_1Rnd_50x137_HEAT, 1);
+            mag_xx(OPTRE_16Rnd_127x40_Mag_NARQT, 1);
+            mag_xx(OPTRE_16Rnd_127x40_Mag_APT, 2);
+            mag_xx(OPTRE_60Rnd_5x23mm_Mag_FMJT, 5);
+            mag_xx(OPTRE_60Rnd_762x51_Mag_APT, 5);
+            mag_xx(3Rnd_HE_Grenade_shell, 2);
+            delete _xx_OPTRE_ELB47_Strobe;
+            delete _xx_OPTRE_M8_Flare;
+            delete _xx_OPTRE_M2_Smoke_Purple;
+		};
+		class TransportWeapons{};
+		class TransportItems
+		{
+            item_xx(ACE_elasticBandage, 8);
+            item_xx(ACE_packingBandage, 14);
+            item_xx(ACE_plasmaIV_500, 2);
+            item_xx(ACE_epinephrine, 3);
+            item_xx(ACE_adenosine, 2);
+            item_xx(ACE_tourniquet, 2);
+            item_xx(ACE_quikclot, 8);
+            item_xx(ACE_morphine, 1);
+            item_xx(ACE_Banana, 5);
+            item_xx(ACE_splint, 2);
+            delete _xx_OPTRE_Biofoam;
+        };
+    };
+    class Praetor_LR_backpack: OPTRE_ANPRC_515
+    {
+        displayName = "[1stMEU] ODST Reconnaissance Rucksack";
+        tf_encryptionCode = "tf_west_radio_code";
+        tf_dialog = "anarc210_radio_dialog";
+        tf_subtype = "digital_lr";
+        tf_range = 30000;
+        tf_dialogUpdate = "call TFAR_fnc_updateLRDialogToChannel;";
+        tf_hasLRradio = 1;
+        maximumLoad = 100;
+    };
+    class Praetor_Jumpack: OPTRE_S12_SOLA_Jetpack	
+    {
+        displayName = "[1stMEU] ODST Advanced Reconnaissance Jumpack";
+        tf_encryptionCode = "tf_west_radio_code";
+        tf_dialog = "anarc210_radio_dialog";
+        tf_subtype = "digital_lr";
+        tf_range = 30000;
+        tf_dialogUpdate = "call TFAR_fnc_updateLRDialogToChannel;";
+        tf_hasLRradio = 1;
+        maximumLoad = 100;
+    };
+    class OPTRE_ANPRC_521_Black;
+    class Praetor_LR_backpack_big: OPTRE_ANPRC_521_Black
+    {
+        displayName = "[1stMEU] ODST LR Rucksack";
+        tf_encryptionCode = "tf_west_radio_code";
+        tf_dialog = "anarc210_radio_dialog";
+        tf_subtype = "digital_lr";
+        tf_range = 30000;
+        tf_dialogUpdate = "call TFAR_fnc_updateLRDialogToChannel;";
+        tf_hasLRradio = 1;
+        maximumLoad = 100;
+    };
 };
