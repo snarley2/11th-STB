5b082b1fb5afb159771124892ba5f0db54cd62cb
Mark <dmarkbroome@gmail.com>
2020-08-04 01:58:51 -0500

    4th beret, some vehicle testing stuff

--- addons/first_meu_aux/CfgVehicles.hpp
+++ addons/first_meu_aux/CfgVehicles.hpp
@@ -48,6 +48,7 @@ class CfgVehicles
     class OPTRE_UNSC_hornet;
 	class OPTRE_M313_UNSC;
     class B_parachute;
+    class OPTRE_AV22_Sparrowhawk_Base;
     
     class MEU_compat_IbuprofenItem: ACE_morphineItem 
     {
@@ -61,7 +62,65 @@ class CfgVehicles
             item_xx(MEU_Ibuprophen,1);
         };
     };
-		
+    
+	class VES_AV22_Sparrowhawk_Base: OPTRE_AV22_Sparrowhawk_Base
+	{
+        tf_range=30000;
+         class pilotCamera
+        {
+            class OpticsIn
+            {
+                class Wide
+                {
+                    opticsDisplayName = "WFOV";
+                    initAngleX = 0;
+                    minAngleX = 0;
+                    maxAngleX = 0;
+                    initAngleY = 0;
+                    minAngleY = 0;
+                    maxAngleY = 0;
+                    initFov = "(75 / 120)";
+                    minFov = "(75 / 120)";
+                    maxFov = "(75 / 120)";
+                    directionStabilized = 1;
+                    visionMode[] = {"Normal","NVG","Ti"};
+                    thermalMode[] = {0,1};
+                    gunnerOpticsModel = "\A3\Drones_F\Weapons_F_Gamma\Reticle\UAV_Optics_Gunner_wide_F.p3d";
+                    opticsPPEffects[] = {"OpticsCHAbera2","OpticsBlur2"};
+                };
+                class Medium: Wide
+                {
+                    opticsDisplayName = "MFOV";
+                    initFov = "(14.4 / 120)";
+                    minFov = "(14.4 / 120)";
+                    maxFov = "(14.4 / 120)";
+                    gunnerOpticsModel = "\A3\Drones_F\Weapons_F_Gamma\Reticle\UAV_Optics_Gunner_medium_F.p3d";
+                };
+                class Narrow: Wide
+                {
+                    opticsDisplayName = "NFOV";
+                    initFov = "(4.8 / 120)";
+                    minFov = "0.01";
+                    maxFov = "(4.8 / 120)";
+                    gunnerOpticsModel = "\A3\Drones_F\Weapons_F_Gamma\Reticle\UAV_Optics_Gunner_narrow_F.p3d";
+                };
+            };
+            minTurn = -90;
+            maxTurn = 90;
+            initTurn = 0;
+            minElev = -10;
+            maxElev = 90;
+            initElev = 25;
+            maxXRotSpeed = 1;
+            maxYRotSpeed = 1;
+            maxMouseXRotSpeed = 0.5;
+            maxMouseYRotSpeed = 0.5;
+            pilotOpticsShowCursor = 1;
+            controllable = 1;
+        };
+        memoryPointDriverOptics = "gunnerview";
+    };
+    
 	class OPTRE_UNSC_falcon: OPTRE_falcon_base
     {
         tf_range=30000;
@@ -836,6 +895,7 @@ class CfgVehicles
     };
     class OPTRE_HEV: StaticMGWeapon
 	{
+        faction = "MEU_UNSC";
         class TransportBackpacks
         {
             class xx_OPTRE_OPTRE_ILCS_HEAVY
